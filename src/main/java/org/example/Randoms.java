bcd100d8af4e6394199d363d0c7f2dde62e465a0
Alexandra <84934027+Blootooz@users.noreply.github.com>
2023-03-09 07:50:35 +0300

    fix commit

--- src/main/java/org/example/Randoms.java
+++ src/main/java/org/example/Randoms.java
@@ -1,15 +1,13 @@
 package org.example;
 
 import java.util.Iterator;
-import java.util.PrimitiveIterator;
 import java.util.Random;
 
 public class Randoms implements Iterable<Integer> {
-    protected PrimitiveIterator.OfInt randomIterator;
-    protected int min, max;
+    private final int min;
+    private final int max;
 
     public Randoms(int min, int max) {
-        randomIterator = new Random().ints(min, (max + 1)).iterator();
         this.min = min;
         this.max = max;
     }
@@ -17,6 +15,8 @@ public class Randoms implements Iterable<Integer> {
     @Override
     public Iterator<Integer> iterator() {
         return new Iterator<>() {
+            private final Random random = new Random();
+
             @Override
             public boolean hasNext() {
                 return true;
@@ -24,7 +24,7 @@ public class Randoms implements Iterable<Integer> {
 
             @Override
             public Integer next() {
-                return randomIterator.nextInt();
+                return random.nextInt(max - min + 1) + min;
             }
         };
     }
